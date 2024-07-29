f30bd8884aef486727e49cced4a32239337fbbe3
Pirate-960 <a.a.zahran.960@gmail.com>
2024-07-30 01:00:38 +0300

    Create index.js

---
+++ Version-2/todo-app/pages/index.js
@@ -0,0 +1,13 @@
+// pages/index.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h2>Welcome to TodoApp</h2>
+      <p>Manage your todos efficiently with our application.</p>
+    </div>
+  );
+};
+
+export default Home;
