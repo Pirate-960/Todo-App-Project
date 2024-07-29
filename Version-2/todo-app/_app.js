122ac396f5fb843af0be8232d6b9ec20409eedd5
Pirate-960 <a.a.zahran.960@gmail.com>
2024-07-30 01:30:57 +0300

    Fix - Run Success!

--- Version-2/todo-app/_app.js
+++ Version-2/todo-app/_app.js
@@ -1,7 +1,7 @@
 // pages/_app.js
 import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Layout from '../components/Layout';
+import Layout from './components/Layout';
 
 function MyApp({ Component, pageProps }) {
   return (
